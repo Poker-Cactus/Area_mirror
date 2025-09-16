051cd03b1282995c67c0d94b1979f24567b59bf5
Marco <marco.regad@epitech.eu>
2025-09-16 11:22:08 +0200

    ADD dynamic about.json with type and current ip

--- server/app.js
+++ server/app.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.get('/about.json', (req, res) => {
-  res.sendFile(path.join(__dirname, 'about.json'));
+  const about = {
+    client: {
+      host: req.ip || req.connection.remoteAddress || "unknown"
+    },
+    server: {
+      current_time: Math.floor(Date.now() / 1000),
+      services: [
+        {
+          name: "facebook",
+          actions: [
+            {
+              name: "new_message_in_group",
+              description: "A new message is posted in the group"
+            },
+            {
+              name: "new_message_inbox",
+              description: "A new private message is received by the user"
+            },
+            {
+              name: "new_like",
+              description: "The user gains a like from one of their messages"
+            }
+          ],
+          reactions: [
+            {
+              name: "like_message",
+              description: "The user likes a message"
+            }
+          ]
+        }
+      ]
+    }
+  };
+  res.json(about);
 });
 
 app.listen(8080, () => {
