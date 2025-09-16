c5b8bc096f6b74d74472ade70bbe78f3fbacd9f4
Erwan <erwan.itier@epitech.eu>
2025-09-16 12:29:26 +0200

    [INIT] nx setup

---
+++ apps/front-web/src/app/app.tsx
@@ -0,0 +1,11 @@
+import './app.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="front-web" />
+    </div>
+  );
+}
+export default App;
